f714495e2e9587536de2fd6149aad53e22d7d49e
MaiCodes-exe <maise.b.costa@gmail.com>
2023-02-07 20:59:50 +0000

    13th ex NEEDS REVIEWING

--- exercices/13.js
+++ exercices/13.js
@@ -1,4 +1,14 @@
 // Destructuring via rest elements
 
 
-NEEDS REVIEWING
+function removeFirstTwo(list) {
+  // Only change code below this line
+  const [a, b, ...shorterList] = list; // Change this line
+  // Only change code above this line
+  return shorterList;
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
+
+
