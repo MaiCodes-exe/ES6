777483461543f84f1fb21f0d29459ce02f5829c6
MaiCodes-exe <maise.b.costa@gmail.com>
2023-02-09 20:46:59 +0000

    16th ex

---
+++ exercices/16.js
@@ -0,0 +1,16 @@
+// Write Concise Object Literal Declarations Using Object Property Shorthand
+
+// possible solution below
+// const createPerson = (name, age, gender) => ({name, age, gender});
+
+
+
+const createPerson = (name, age, gender) => {
+  // change code below this line
+  return {
+    name,
+    age,
+    gender
+  };
+  // change code above this line
+};
