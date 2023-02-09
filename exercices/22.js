35486f62f50ef7213befeefe7fe4aa05a59b2833
MaiCodes-exe <maise.b.costa@gmail.com>
2023-02-09 21:34:20 +0000

    22nd ex

---
+++ exercices/22.js
@@ -0,0 +1,8 @@
+// Reuse JavaScript Code Using import
+
+import { uppercaseString, lowercaseString } from './string_functions.js';
+
+// Only change code above this line
+
+uppercaseString("hello");
+lowercaseString("WORLD!");
